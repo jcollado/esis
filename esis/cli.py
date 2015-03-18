6d5c438a76dab62fbdf3c50625d55cb8a9eddbea
Javier Collado <jcollado@nowsecure.com>
2015-03-18 20:05:06 +0100

    Add ability to set log level

--- esis/cli.py
+++ esis/cli.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 def main():
     """Entry point for the esis.py script."""
     args = parse_arguments()
+    configure_logging(args.log_level)
     args.func(args)
 
 def index(args):
@@ -32,6 +33,25 @@ def valid_directory(path):
 
     return path
 
+def configure_logging(log_level):
+    """Configure logging based on command line argument.
+
+    :param log_level: Log level passed form the command line
+    :type log_level: int
+
+    """
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    # Log to sys.stderr using log level
+    # passed through command line
+    log_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+    log_handler.setFormatter(formatter)
+    log_handler.setLevel(log_level)
+    root_logger.addHandler(log_handler)
+
+
 def parse_arguments():
     """Parse command line arguments.
 
@@ -40,14 +60,28 @@ def parse_arguments():
 
     """
     parser = argparse.ArgumentParser(description=__doc__)
+    log_levels = ['debug', 'info', 'warning', 'error', 'critical']
+    parser.add_argument(
+        '-l', '--log-level',
+        dest='log_level',
+        choices=log_levels,
+        default='warning',
+        help=('Log level. One of {0} or {1} '
+              '(%(default)s by default)'
+              .format(', '.join(log_levels[:-1]), log_levels[-1])))
+
     subparsers = parser.add_subparsers(help='Subcommands')
+
     index_parser = subparsers.add_parser('index', help='Index SQLite database files')
     index_parser.add_argument('directory', type=valid_directory, help='Base directory')
     index_parser.set_defaults(func=index)
+
     search_parser = subparsers.add_parser('search', help='Search indexed data')
     search_parser.add_argument('query', help='Search query')
     search_parser.set_defaults(func=search)
+
     args = parser.parse_args()
+    args.log_level = getattr(logging, args.log_level.upper())
     return args
 
 if __name__ == '__main__':
