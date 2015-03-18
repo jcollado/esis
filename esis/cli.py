1cfaf387af8e373d2bf3fdc8d6144f889489ba13
Javier Collado <jcollado@nowsecure.com>
2015-03-18 18:32:00 +0100

    Add directory validation to argument parsing

--- esis/cli.py
+++ esis/cli.py
@@ -2,12 +2,25 @@
 """Elastic Search Index & Search."""
 
 import argparse
+import os
 
 
 def main():
     """Entry point for the esis.py script."""
     args = parse_arguments()
     print args
+
+def valid_directory(path):
+    """Directory validation."""
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(
+            '{!r} is not a valid directory'.format(path))
+
+    if not os.access(path, os.R_OK | os.X_OK):
+        raise argparse.ArgumentTypeError(
+            'not enough permissions to explore {!r}'.format(path))
+
+    return path
 
 def parse_arguments():
     """Parse command line arguments.
@@ -19,7 +32,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description=__doc__)
     subparsers = parser.add_subparsers(help='Subcommands')
     index = subparsers.add_parser('index', help='Index SQLite database files')
-    index.add_argument('directory', help='Base directory')
+    index.add_argument('directory', type=valid_directory, help='Base directory')
     search = subparsers.add_parser('search', help='Search indexed data')
     search.add_argument('query', help='Search query')
     args = parser.parse_args()
