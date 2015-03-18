99f21681c81ab67f0be5d6265d09af983599cc08
Javier Collado <jcollado@nowsecure.com>
2015-03-18 19:58:07 +0100

    Add handlers for the index/search subcommands

--- esis/cli.py
+++ esis/cli.py
@@ -2,13 +2,23 @@
 """Elastic Search Index & Search."""
 
 import argparse
+import logging
 import os
 
+logger = logging.getLogger(__name__)
 
 def main():
     """Entry point for the esis.py script."""
     args = parse_arguments()
-    print args
+    args.func(args)
+
+def index(args):
+    """Index database information into elasticsearch."""
+    logger.debug('Indexing %r...', args.directory)
+
+def search(args):
+    """Send query to elasticsearch."""
+    logger.debug('Searching %r...', args.query)
 
 def valid_directory(path):
     """Directory validation."""
@@ -31,10 +41,12 @@ def parse_arguments():
     """
     parser = argparse.ArgumentParser(description=__doc__)
     subparsers = parser.add_subparsers(help='Subcommands')
-    index = subparsers.add_parser('index', help='Index SQLite database files')
-    index.add_argument('directory', type=valid_directory, help='Base directory')
-    search = subparsers.add_parser('search', help='Search indexed data')
-    search.add_argument('query', help='Search query')
+    index_parser = subparsers.add_parser('index', help='Index SQLite database files')
+    index_parser.add_argument('directory', type=valid_directory, help='Base directory')
+    index_parser.set_defaults(func=index)
+    search_parser = subparsers.add_parser('search', help='Search indexed data')
+    search_parser.add_argument('query', help='Search query')
+    search_parser.set_defaults(func=search)
     args = parser.parse_args()
     return args
 
