5bf5151541b2aa72b099c1f2a1e33a0d83464393
Javier Collado <jcollado@nowsecure.com>
2015-04-10 00:04:32 +0200

    Add command functions tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """Command Line Interface test cases."""
 
+import argparse
 import os
 import tempfile
 import unittest
+
+from mock import patch
 
 from esis.cli import (
     clean,
@@ -12,6 +15,50 @@ from esis.cli import (
     parse_arguments,
     search,
 )
+
+class CommandFunctionTests(unittest.TestCase):
+
+    """Command function test cases."""
+
+    def setUp(self):
+        """Patch elasticsearch client."""
+        self.patcher = patch('esis.cli.Client')
+        client_cls = self.patcher.start()
+        self.client = client_cls()
+
+    def test_index(self):
+        """Index command function."""
+        directory = 'some directory'
+        args = argparse.Namespace(directory=directory)
+        index(args)
+        self.client.index.assert_called_once_with(directory)
+
+    def test_search(self):
+        """Search command function."""
+        query = 'some query'
+        args = argparse.Namespace(query=query)
+        self.client.search.side_effect = [
+            ['result_1', 'result_2'],
+            ['result_3', 'result_4'],
+        ]
+        search(args)
+        self.client.search.assert_called_once_with(query)
+
+    def test_count(self):
+        """Count command function."""
+        args = argparse.Namespace()
+        count(args)
+        self.client.count.assert_called_once_with()
+
+    def test_clean(self):
+        """Clean command function."""
+        args = argparse.Namespace()
+        clean(args)
+        self.client.clean.assert_called_once_with()
+
+    def tearDown(self):
+        """Undo the patching."""
+        self.patcher.stop()
 
 
 class ParseArgumentsTest(unittest.TestCase):
