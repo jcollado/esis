73fe56f9bf869694768e8c184137ed439ff98704
Javier Collado <jcollado@nowsecure.com>
2015-04-09 21:33:17 +0200

    Add client tests

--- tests/test_es.py
+++ tests/test_es.py
@@ -1,16 +1,192 @@
 # -*- coding: utf-8 -*-
 """Elasticsearch client test cases."""
 
+import hashlib
 import tempfile
 import unittest
 
+from mock import (
+    MagicMock as Mock,
+    patch,
+)
 from sqlalchemy import types as sql_types
 
 from esis.es import (
+    Client,
     Mapping,
     get_document,
     get_index_action,
 )
+
+
+class ClientTest(unittest.TestCase):
+
+    """Elasticsearch client wrapper test cases."""
+
+    def setUp(self):
+        """Patch elasticsearch class and create client object."""
+        self.patcher = patch('esis.es.Elasticsearch')
+        self.elasticsearch_cls = self.patcher.start()
+        self.client = Client()
+
+    def test_index(self):
+        """Index directory wrapper."""
+        directory = 'some directory'
+        self.client._index_directory = Mock()
+
+        self.client.index(directory)
+        self.client._index_directory.assert_called_once_with(directory)
+
+    @patch('esis.es.Database')
+    @patch('esis.es.TreeExplorer')
+    def test_index_directory(self, tree_explorer_cls, database_cls):
+        """Index directory."""
+        tree_explorer_cls().paths.return_value = ['path_1', 'path_2', 'path_3']
+        self.client._recreate_index = Mock()
+        documents_indexed_per_database = [1, 2, 3]
+        self.client._index_database = Mock(
+            side_effect=documents_indexed_per_database)
+
+        directory = 'some directory'
+        self.assertEqual(
+            self.client._index_directory(directory),
+            sum(documents_indexed_per_database),
+        )
+
+    def test_recreate_index_that_exists(self):
+        """Index is deleted and then created."""
+        indices = self.elasticsearch_cls().indices
+        indices.exists.return_value = True
+
+        index_name = 'abcd'
+        self.client._recreate_index(index_name)
+        indices.delete.assert_called_once_with(index_name)
+        indices.create.assert_called_once_with(index_name)
+
+    def test_recreate_index_that_does_not_exist(self):
+        """Index is created."""
+        indices = self.elasticsearch_cls().indices
+        indices.exists.return_value = False
+
+        index_name = 'abcd'
+        self.client._recreate_index(index_name)
+        self.assertFalse(indices.delete.called)
+        indices.create.assert_called_once_with(index_name)
+
+    @patch('esis.es.DBReader')
+    def test_index_database(self, db_reader_cls):
+        """Index a database completely."""
+        index_name = 'abcd-efgh'
+        database = Mock()
+        db_reader = db_reader_cls()
+        db_reader.tables.return_value = ('calls', 'messages', 'pictures')
+        documents_indexed_per_table = (1, 2, 3)
+        self.client._index_table = Mock(
+            side_effect=documents_indexed_per_table)
+
+        documents_indexed = self.client._index_database(index_name, database)
+        db_reader_cls.assert_called_with(database)
+        self.assertEqual(documents_indexed, sum(documents_indexed_per_table))
+
+    @patch('esis.es.elasticsearch.helpers.bulk')
+    @patch('esis.es.Mapping')
+    def test_index_table(self, mapping_cls, bulk_mock):
+        """Index a database table."""
+        indices = self.elasticsearch_cls().indices
+        mapping = mapping_cls()
+        rows = [
+            {'id': 1, 'number': '123456789'},
+            {'id': 2, 'number': '234567890'},
+            {'id': 3, 'number': '345678901'},
+        ]
+        bulk_mock.return_value = (len(rows), [])
+
+        index_name = 'abcd-efgh'
+        table_name = 'calls'
+        table_reader = Mock()
+        table_reader.rows.return_value = rows
+        documents_indexed = self.client._index_table(
+            index_name, table_name, table_reader)
+        indices.put_mapping.assert_called_once_with(
+            index=index_name,
+            doc_type=hashlib.md5(table_name).hexdigest(),
+            body=mapping.mapping)
+        self.assertEqual(documents_indexed, len(rows))
+
+    @patch('esis.es.elasticsearch.helpers.bulk')
+    @patch('esis.es.Mapping')
+    def test_index_table_with_some_failures(self, mapping_cls, bulk_mock):
+        """Index a database table with some failures handled."""
+        indices = self.elasticsearch_cls().indices
+        mapping = mapping_cls()
+        rows = [
+            {'id': 1, 'number': '123456789'},
+            {'id': 2, 'number': '234567890'},
+            {'id': 3, 'number': '345678901'},
+        ]
+        indexed_rows = 1
+        bulk_mock.return_value = (indexed_rows, ['some error'])
+
+        index_name = 'abcd-efgh'
+        table_name = 'calls'
+        table_reader = Mock()
+        table_reader.rows.return_value = rows
+        documents_indexed = self.client._index_table(
+            index_name, table_name, table_reader)
+        indices.put_mapping.assert_called_once_with(
+            index=index_name,
+            doc_type=hashlib.md5(table_name).hexdigest(),
+            body=mapping.mapping)
+        self.assertEqual(
+            documents_indexed,
+            indexed_rows,
+        )
+
+    def test_search(self):
+        """Search using text query."""
+        self.elasticsearch_cls().search.return_value = {
+            'hits': {
+                'total': 10,
+                'hits': [1, 2, 3, 4],
+            },
+            '_scroll_id': 'abcd'
+        }
+        self.elasticsearch_cls().scroll.side_effect = [
+            {'hits': {'hits': [5, 6, 7]}},
+            {'hits': {'hits': [8, 9, 10]}},
+            {'hits': {'hits': []}},
+        ]
+
+        query = 'this is the query'
+        hits = list(self.client.search(query))
+
+        self.assertListEqual(
+            hits,
+            [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]])
+
+    def test_clean(self):
+        """Clean indices."""
+        self.client.clean()
+        indices = self.elasticsearch_cls().indices
+        indices.delete.assert_called_once_with(index='_all')
+
+    def test_count(self):
+        """Return indexed documents."""
+        expected_count = {
+            u'_shards': {
+                u'failed': 0,
+                u'successful': 0,
+                u'total': 0,
+            },
+            u'count': 0,
+        }
+
+        self.elasticsearch_cls().count.return_value = expected_count
+        self.assertDictEqual(self.client.count(), expected_count)
+
+    def tearDown(self):
+        """Undo the patching."""
+        self.patcher.stop()
 
 
 class MappingTest(unittest.TestCase):
