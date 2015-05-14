b700cc013be2236c50937876b974891355842782
Javier Collado <jcollado@nowsecure.com>
2015-05-14 23:08:13 +0200

    Move import to the top of the file

--- esis/__init__.py
+++ esis/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """Elastic Search Index & Search."""
 
+from esis.es import Client
+
 __author__ = 'Javier Collado'
 __email__ = 'jcollado@nowsecure.com'
 __version__ = '0.2.0'
-
-from esis.es import Client
